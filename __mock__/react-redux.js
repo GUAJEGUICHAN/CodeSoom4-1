b8a5a4e1835db1381e363340a6a4bc8ca968e105
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-21 14:54:06 +0900

    [test] useSelector라는 가상 함수를 만들었다. 매개변수로 함수를 받는다. 그 함수에 task를 넣는 것이다.
    테스트 때 app컴포넌트 안에 있는 selector 함수를 사용한다.

--- __mock__/react-redux.js
+++ __mock__/react-redux.js
@@ -1,3 +1,11 @@
 export const useDispatch = jest.fn();
 
-export const useSelector = jest.fn();
+export const useSelector = jest.fn(
+  (selector) =>
+    selector({
+      tasks: [
+        { id: 1, title: '아무것도 안하기' },
+        { id: 2, title: '책 읽기' },
+      ],
+    })
+);
